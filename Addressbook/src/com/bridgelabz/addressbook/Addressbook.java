7b1c64720144c5082c41375c2b7a995c82140d28
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-24 23:37:58 +0530

    welcome

---
+++ Addressbook/src/com/bridgelabz/addressbook/Addressbook.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbook;
+
+public class Addressbook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("welcome to address book system");
+	}
+
+}
